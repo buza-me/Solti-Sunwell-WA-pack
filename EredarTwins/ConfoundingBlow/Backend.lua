ca661448cf527d03880f4c9242561a8438059ab1
kbuza <buza.work@gmail.com>
2024-05-31 23:45:02 +0300

    Added conditions to the Twins ConfoundingBlow backend to show the notifications only in second phase or in normal raid difficulty.

--- EredarTwins/ConfoundingBlow/Backend.lua
+++ EredarTwins/ConfoundingBlow/Backend.lua
@@ -6,6 +6,12 @@ function Init()
   aura_env.TRACKED_SPELL_ID = 45256
   --aura_env.TRACKED_SPELL_ID = 25222 -- Renew test
   aura_env.BUFF_DURATION = 6
+  aura_env.phase = 1
+  aura_env.AFFINITY_DEBUFFS = {
+    [22442] = true, -- fire affinity
+    [9657] = true,  -- shadow affinity
+  }
+  aura_env.BOSS_PHASE_MESSAGE = "Magic Affinity has been disrupted!"
   aura_env.TRIGGER_EVENT = "SOLTI_CONFOUNDING_BLOW_TRIGGER"
 end
 
@@ -27,16 +33,27 @@ function Trigger1(
 )
   local shouldAbort =
       event == "OPTIONS"
-      or spellID ~= aura_env.TRACKED_SPELL_ID
       or not UnitExists(destName)
+      or (
+        spellID ~= aura_env.TRACKED_SPELL_ID
+        and not aura_env.AFFINITY_DEBUFFS[spellID]
+      )
 
   if shouldAbort then
     return false
   end
 
-  local duration = 0
+  if subEvent == "SPELL_AURA_APPLIED" and aura_env.AFFINITY_DEBUFFS[spellID] then
+    aura_env.phase = 1
+    return false
+  end
 
-  if subEvent == "SPELL_AURA_APPLIED" then
+  local duration = 0
+  local shouldDisplay =
+      subEvent == "SPELL_AURA_APPLIED"
+      and (aura_env.phase == 2 or not aura_env.CONTEXT.IsHeroic())
+
+  if shouldDisplay then
     duration = aura_env.BUFF_DURATION
   end
 
@@ -46,6 +63,21 @@ function Trigger1(
     duration,
     aura_env.CONTEXT:IsMyName(destName)
   )
+
+  return false
+end
+
+-- CHAT_MSG_MONSTER_EMOTE,CHAT_MSG_RAID_BOSS_EMOTE
+function Trigger2(event, message, sourceName, languageName, channelName, targetName)
+  local shouldAbort =
+      event == "OPTIONS"
+      or message ~= aura_env.BOSS_PHASE_MESSAGE
+
+  if shouldAbort then
+    return false
+  end
+
+  aura_env.phase = 2
 
   return false
 end
